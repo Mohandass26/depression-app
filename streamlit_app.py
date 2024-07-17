c6c3022326aab9f03093e173eae678f78b3085d6
Mohandass26 <157234695+Mohandass26@users.noreply.github.com>
2024-07-17 18:55:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if selected == "Home":
 
     # Render a button to navigate to Classify Depression page
     if st.button("CLICK"):
-        Classify_Depression.main()
+        st.experimental_set_query_params(selected="Classify Depression")
 
 elif selected == "Classify Depression":
     Classify_Depression.main()
