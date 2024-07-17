e73a1e757d49ab55db25063d4d9b482287ded533
Mohandass26 <157234695+Mohandass26@users.noreply.github.com>
2024-07-17 17:29:32 +0800

    Update Contact.py

--- Contact.py
+++ Contact.py
@@ -1,29 +1,30 @@
-
+# contact.py
 import streamlit as st
 
-st.markdown('<h1 style="color: orange;">Contact Help</h1>', unsafe_allow_html=True)
+def main():
+    st.markdown('<h1 style="color: orange;">Contact Help</h1>', unsafe_allow_html=True)
 
-st.markdown('<h3 style="color: orange;">Malaysian Mental Health Association (MMHA)</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 style="color: orange;">Malaysian Mental Health Association (MMHA)</h3>', unsafe_allow_html=True)
 
-st.write("**Email:** info@mmha.org.my")
-st.write("**Contact:** +60 3-2780 6803")
+    st.write("**Email:** info@mmha.org.my")
+    st.write("**Contact:** +60 3-2780 6803")
 
-st.markdown("""
-For more information and resources, you can visit the [Malaysian Mental Health Association (MMHA) website](https://www.mmha.org.my).
-""")
+    st.markdown("""
+    For more information and resources, you can visit the [Malaysian Mental Health Association (MMHA) website](https://www.mmha.org.my).
+    """)
 
-st.markdown("---")
+    st.markdown("---")
 
-st.markdown('<h3 style="color: orange;">UKM Counselling</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 style="color: orange;">UKM Counselling</h3>', unsafe_allow_html=True)
 
-st.write("**Address:**")
-st.write("Pusat Hal Ehwal Pelajar (HEP-UKM)")
-st.write("Aras 7, Bangunan PUSANIKA")
-st.write("43600 UKM, Bangi Selangor, MALAYSIA")
+    st.write("**Address:**")
+    st.write("Pusat Hal Ehwal Pelajar (HEP-UKM)")
+    st.write("Aras 7, Bangunan PUSANIKA")
+    st.write("43600 UKM, Bangi Selangor, MALAYSIA")
 
-st.write("**Email:** hep@ukm.edu.my")
-st.write("**Contact:** +603-8921 5347")
+    st.write("**Email:** hep@ukm.edu.my")
+    st.write("**Contact:** +603-8921 5347")
 
-st.markdown("""
-For more information and resources, you can visit the [UKM Counseling Unit website](https://www.ukm.my/hepukm/unit-kaunseling-2/).
-""")
+    st.markdown("""
+    For more information and resources, you can visit the [UKM Counseling Unit website](https://www.ukm.my/hepukm/unit-kaunseling-2/).
+    """)
