0c9dd4c779011f1fbda1d0e48aeb283058608f3a
Mohandass26 <157234695+Mohandass26@users.noreply.github.com>
2024-07-17 20:27:11 +0800

    Update Classify_Depression.py

--- Classify_Depression.py
+++ Classify_Depression.py
@@ -123,8 +123,8 @@ def main():
 
             if prediction == "Depressed":
                 with col2:
-                    if st.button("NEXT"):
-                          Contact.main()
+                    if st.button("NEXT",Contact.main()):
+                         
                         
     st.markdown("---")
 
